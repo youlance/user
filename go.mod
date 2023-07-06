aa3751209e8bf5944bfa39db9d42ae4181682f01
Mehdi Eidi <mehdiadq@gmail.com>
2023-07-06 19:01:06 +0330

    refactor: add auto migration

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/lib/pq v1.10.6
 	github.com/spf13/viper v1.12.0
 	golang.org/x/crypto v0.11.0
@@ -19,6 +20,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
@@ -37,6 +40,7 @@ require (
 	github.com/subosito/gotenv v1.3.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/arch v0.4.0 // indirect
 	golang.org/x/net v0.12.0 // indirect
 	golang.org/x/sys v0.10.0 // indirect
--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
+
+	"errors"
 
 	server "github.com/youlance/user/api/http"
 	db "github.com/youlance/user/db/sqlc"
 	"github.com/youlance/user/pkg/config"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -17,11 +23,14 @@ func main() {
 	}
 
 	DSN := "postgresql://" + config.DBUser + ":" + config.DBPass + "@" + config.DBAddress + "/" + config.DBName + "?sslmode=disable"
-	fmt.Print(DSN)
 
 	conn, err := sql.Open(config.DBDriver, DSN)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
+	}
+
+	if err := migrateUp(conn, "file://migration"); err != nil {
+		panic(err)
 	}
 
 	store := db.New(conn)
@@ -34,4 +43,28 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
+}
+
+func migrateUp(db *sql.DB, migrationsPath string) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		migrationsPath,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil {
+		if !errors.Is(err, migrate.ErrNoChange) {
+			return err
+		}
+	}
+
+	return nil
 }
